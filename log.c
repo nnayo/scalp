61a265f81a850bb950515ed9fba67a877ff9adae
yog <yog@fa5d5384-1a15-4032-82b6-d6ed0b1f1c97>
2009-12-29 16:56:56 +0000

    major updates
    git-svn-id: svn://uranus/scalp/trunk@28 fa5d5384-1a15-4032-82b6-d6ed0b1f1c97

--- log.c
+++ log.c
@@ -27,17 +27,23 @@
 #include "utils/fifo.h"
 #include "utils/time.h"
 
+#include "drivers/eeprom.h"
+#include "drivers/twi.h"
+
 #include "avr/io.h"
+
+#include <string.h>		// memset()
 
 
 //----------------------------------------
 // private defines
 //
 
-#define NB_FRAMES	10
+#define NB_FRAMES	7
 
-#define MINIMAL_FILTER	_CM(FR_EEP_READ) | _CM(FR_EEP_WRITE) | _CM(FR_LOG_CMD)
+#define MINIMAL_FILTER	_CM(FR_LOG_CMD)
 
+#define NB_ORIG_FILTER	6
 
 //----------------------------------------
 // private defines
@@ -49,6 +55,12 @@ typedef struct {
 	dpt_frame_t fr;		// complete frame
 } log_t;
 
+typedef enum {
+	LOG_OFF,
+	LOG_SDCARD,
+	LOG_EEPROM,
+} log_state_t;
+
 
 //----------------------------------------
 // private variables
@@ -56,21 +68,21 @@ typedef struct {
 
 static struct {
 	dpt_interface_t interf;		// dispatcher interface
-	pt_t	pt;					// context
-	pt_t	pt2;				// spawn context
-	pt_t	pt3;				// spawn context
+	pt_t	log_pt;				// context
 
 	fifo_t	in_fifo;			// reception fifo
 	dpt_frame_t in_buf[NB_FRAMES];
+	dpt_frame_t fr;				// command frame 
 
-	dpt_frame_t fr_in;			// incoming frame
-	dpt_frame_t fr_out;			// outgoing frame
-	dpt_frame_t fr_filter;		// filtered frame 
+	log_state_t state;			// logging state
 
-	u8 enable;					// logging state
-
-	u16	addr;					// address in eeprom
+	u16	eeprom_addr;			// address in eeprom
+	u64	sdcard_addr;			// address in sdcard
 	u8	index;					// session index
+
+	u8 orig_filter[NB_ORIG_FILTER];	// origin node filter
+
+	log_t block;
 } LOG;
 
 
@@ -78,24 +90,115 @@ static struct {
 // private functions
 //
 
+// find the start address the new logging session
+// and return the log index found in eeprom
+static u8 LOG_find_eeprom_start(void)
+{
+	u8 buf[2];
+
+	// scan the whole eeprom
+	while (1) {
+		// read the 2 first octets of the log at LOG.addr
+		EEP_read(LOG.eeprom_addr, buf, sizeof(buf));
+
+		// if the read octets are erased eeprom
+		if ( (buf[0] == 0xff) && (buf[1] == 0xff) ) {
+			// the new log session start address is found (here)
+			//
+			// the previous index is known from the previous read (see below)
+			// so increment it
+			LOG.index++;
+
+			// finally the scan is over
+			return LOG.index;
+		}
+
+		// extract index
+		LOG.index = buf[0];
+	
+		// check the next log block
+		LOG.eeprom_addr += sizeof(log_t);
+
+		// if address is out of range
+		if ( LOG.eeprom_addr >= EEPROM_END_ADDR ) {
+			// eeprom is full
+			// so give up
+			// the log thread protection will prevent overwriting
+			return 0xff;
+		}
+	}
+}
+
+
+// find the start address in sdcard for this new logging session
+// and set the common log index for eeprom and sdcard
+static void LOG_find_sdcard_start(u8 eeprom_index)
+{
+	u8 buf[2];
+
+	// scan the whole eeprom
+	while (1) {
+		// read the 2 first octets of the log at LOG.addr
+		//EEP_read(LOG.sdcard_addr, buf, sizeof(buf));
+
+		// if the read octets are erased eeprom
+		if ( (buf[0] == 0xff) && (buf[1] == 0xff) ) {
+			// the new log session start address is found (here)
+			//
+			// the previous index is known from the previous read (see below)
+			// so increment it
+			LOG.index++;
+
+			// index found during eeprom scanning is higher
+			if ( eeprom_index > LOG.index ) {
+				// use the eeprom index
+				LOG.index = eeprom_index;
+			}
+
+			// finally the scan is over
+			return;
+		}
+
+		// extract index
+		LOG.index = buf[0];
+	
+		// check the next log block
+		LOG.sdcard_addr += sizeof(log_t);
+
+		// if address is out of range
+		if ( LOG.sdcard_addr >= SDCARD_END_ADDR ) {
+			// sdcard is full
+			// so give up
+			// the log thread protection will prevent overwriting
+			LOG.index = 0xff;
+		}
+	}
+}
+
+
 static void LOG_command(dpt_frame_t* fr)
 {
-	u32 filter;
+	u64 filter;
 
 	// upon the sub-command
 	switch ( fr->argv[0] ) {
 	case 0x00:	// off
-		LOG.enable = FALSE;
+		LOG.state = LOG_OFF;
 		break;
 
-	case 0xff:	// ON
-		LOG.enable = TRUE;
+	case 0x1a:	// log to sdcard
+		LOG.state = LOG_SDCARD;
 		break;
 
-	case 0xa1:	// AND filter MSB value with current MSB value
-		filter = LOG.interf.cmde_mask;
-		filter &= (u32)fr->argv[1] << 24;
-		filter &= (u32)fr->argv[2] << 16;
+	case 0x1e:	// log to eeprom
+		LOG.state = LOG_EEPROM;
+		break;
+
+	case 0x27:	// set command filter LSB part
+		filter  = (u64)fr->argv[1] << 24;
+		filter &= (u64)fr->argv[2] << 16;
+		filter &= (u64)fr->argv[3] <<  8;
+		filter &= (u64)fr->argv[4] <<  0;
 
 		// preserve the logging basic communication
 		filter |= MINIMAL_FILTER;
@@ -104,10 +207,11 @@ static void LOG_command(dpt_frame_t* fr)
 		LOG.interf.cmde_mask = filter;
 		break;
 
-	case 0xa0:	// AND filter LSB value with current LSB value
-		filter = LOG.interf.cmde_mask;
-		filter &= (u32)fr->argv[1] << 8;
-		filter &= (u32)fr->argv[2] << 0;
+	case 0x28:	// set command filter MSB part
+		filter  = (u64)fr->argv[1] << 56;
+		filter &= (u64)fr->argv[2] << 48;
+		filter &= (u64)fr->argv[3] << 40;
+		filter &= (u64)fr->argv[4] << 32;
 
 		// preserve the logging basic communication
 		filter |= MINIMAL_FILTER;
@@ -116,28 +220,32 @@ static void LOG_command(dpt_frame_t* fr)
 		LOG.interf.cmde_mask = filter;
 		break;
 
-	case 0x51:	// OR filter MSB value with current MSB value
+	case 0x2e:	// get command filter LSB part
+		// get the filter value
 		filter = LOG.interf.cmde_mask;
-		filter |= (u32)fr->argv[1] << 24;
-		filter |= (u32)fr->argv[2] << 16;
 
-		// preserve the logging basic communication
-		filter |= MINIMAL_FILTER;
-
-		// set the new filter value
-		LOG.interf.cmde_mask = filter;
+		fr->argv[1] = (u8)(filter >> 24);
+		fr->argv[2] = (u8)(filter >> 16);
+		fr->argv[3] = (u8)(filter >>  8);
+		fr->argv[4] = (u8)(filter >>  0);
 		break;
 
-	case 0x50:	// OR filter LSB value with current LSB value
+	case 0x2f:	// get command filter MSB part
+		// get the filter value
 		filter = LOG.interf.cmde_mask;
-		filter |= (u32)fr->argv[1] << 8;
-		filter |= (u32)fr->argv[2] << 0;
 
-		// preserve the logging basic communication
-		filter |= MINIMAL_FILTER;
+		fr->argv[1] = (u8)(filter >> 56);
+		fr->argv[2] = (u8)(filter >> 48);
+		fr->argv[3] = (u8)(filter >> 40);
+		fr->argv[4] = (u8)(filter >> 32);
+		break;
 
-		// set the new filter value
-		LOG.interf.cmde_mask = filter;
+	case 0x3c:	// set origin filter
+		memcpy(LOG.orig_filter, &fr->argv[1], NB_ORIG_FILTER);
+		break;
+
+	case 0x3f:	// get origin filter
+		memcpy(&fr->argv[1], LOG.orig_filter, NB_ORIG_FILTER);
 		break;
 
 	default:
@@ -151,199 +259,107 @@ static void LOG_command(dpt_frame_t* fr)
 }
 
 
-static PT_THREAD( LOG_write(pt_t* pt) )
-{
-	PT_BEGIN(pt);
-
-	// send the prepared frame
-	PT_WAIT_UNTIL(pt, OK == DPT_tx(&LOG.interf, &LOG.fr_out));
-
-	// wait for the response
-
-	PT_END(pt);
-}
-
-
-static PT_THREAD( LOG_find_start(pt_t* pt) )
-{
-	u16 addr;
-	PT_BEGIN(pt);
-
-	PT_WAIT_UNTIL(pt, FIFO_get(&LOG.in_fifo , &LOG.fr_out));
-
-	// during the search for the log start,
-	// eeprom read response frames must received and treated
-	addr = LOG.fr_out.argv[0] << 8;
-	addr += LOG.fr_out.argv[1] << 0;
-
-	if ( (LOG.fr_out.cmde == FR_EEP_READ) && LOG.fr_out.resp && (LOG.addr == addr) ) {
-		// as it is not an event to log, quit
-		PT_RESTART(pt);
-	}
-
-	// read the 2 first octets of the log at LOG.addr
-	DPT_HEADER(LOG.fr_out, DPT_SELF_ADDR, DPT_SELF_ADDR, FR_EEP_READ, 0, 0, 0, 0)
-	LOG.fr_out.argv[0] = (u8)((LOG.addr & 0xff00) >> 8);
-	LOG.fr_out.argv[1] = (u8)((LOG.addr & 0x00ff) >> 0);
-
-	PT_SPAWN(pt, &LOG.pt3, LOG_write(&LOG.pt3));
-
-	// if the read octets are erased eeprom
-	if ( (LOG.fr_in.argv[2] == 0xff) && (LOG.fr_in.argv[3] == 0xff) ) {
-		// the new log session start address is found (here)
-		//
-		// the previous index is known from the previous read (see below)
-		// so increment it
-		LOG.index++;
-
-		// finally the scan is over
-		PT_EXIT(pt);
-	}
-
-	// extract index
-	LOG.index = (LOG.fr_in.argv[2] & 0xf0) >> 4;
-	
-	// check the next log head
-	LOG.addr += sizeof(log_t);
-
-	// if address is out of range
-	if ( LOG.addr >= END_ADDR ) {
-		// eeprom is full
-		// so give up
-		// the log thread protection will prevent overwriting
-		PT_EXIT(pt);
-	}
-
-	// restart the check to the new address
-	PT_RESTART(pt);
-
-	PT_END(pt);
-}
-
-
 static PT_THREAD( LOG_log(pt_t* pt) )
 {
-	union {
-		u32 full;
-		u8 part[4];
-	} time;
-	u16 addr;
+	u32 time;
+	u8 is_filtered;
+	u8 i;
 
 	PT_BEGIN(pt);
 
-	// if address is out of range
-	if ( LOG.addr >= END_ADDR ) {
-		// logging is no more possible
-		// so quit
-		PT_EXIT(pt);
+	switch ( LOG.state ) {
+		case LOG_OFF:
+		default:
+			// empty the log fifo
+			(void)FIFO_get(&LOG.in_fifo, &LOG.fr);
+
+			// loop back for next frame
+			PT_RESTART(pt);
+			break;
+
+		case LOG_EEPROM:
+			// if address is out of range
+			if ( LOG.eeprom_addr >= EEPROM_END_ADDR ) {
+				// logging is no more possible
+				// so quit
+				PT_EXIT(pt);
+			}
+			break;
+
+		case LOG_SDCARD:
+			// if address is out of range
+			if ( LOG.sdcard_addr >= SDCARD_END_ADDR ) {
+				// logging is no more possible
+				// so quit
+				PT_EXIT(pt);
+			}
+			break;
 	}
 
 	// wait while no frame is present in the fifo
-	PT_WAIT_WHILE(pt, KO == FIFO_get(&LOG.in_fifo, &LOG.fr_filter));
+	PT_WAIT_WHILE(pt, KO == FIFO_get(&LOG.in_fifo, &LOG.fr));
 
-	// but during the search for the log start,
-	// eeprom read response frames must received and treated
-	addr = LOG.fr_filter.argv[0] << 8;
-	addr += LOG.fr_filter.argv[1] << 0;
-
-	// and during the logging,
-	// eeprom write response frames must be received and treated
-	if ( (LOG.fr_filter.cmde == FR_EEP_WRITE) && LOG.fr_filter.resp && (LOG.addr == addr) ) {
-		// as it is not an event to log, quit
-		PT_RESTART(pt);
-	}
 	// if it is a log command
-	if ( (LOG.fr_filter.cmde == FR_LOG_CMD) && (!LOG.fr_filter.resp) ) {
+	if ( (LOG.fr.cmde == FR_LOG_CMD) && (!LOG.fr.resp) ) {
 		// treat it
-		LOG_command(&LOG.fr_filter);
+		LOG_command(&LOG.fr);
 
 		// send the response
-		PT_WAIT_UNTIL(pt, OK == DPT_tx(&LOG.interf, &LOG.fr_filter));
+		PT_WAIT_UNTIL(pt, OK == DPT_tx(&LOG.interf, &LOG.fr));
 
 		// and wait till the next frame
 		PT_RESTART(pt);
 	}
 
-	// if logging is disabled
-	if ( !LOG.enable ) {
-		// wait till the next frame
+	// filter the frame according to its origin
+	is_filtered = OK;	// by default, every frame is filtered
+	for ( i = 0; i < sizeof(LOG.orig_filter); i++ ) {
+		// passthrough or frame origin and filter acceptance match
+		if ( (LOG.orig_filter[i] == 0x00) || (LOG.orig_filter[i] == LOG.fr.orig) ){
+			is_filtered = KO;
+			break;
+		}
+	}
+
+	// if frame is filtered
+	if ( is_filtered ) {
+		// lop back for next frame
 		PT_RESTART(pt);
 	}
 
-	// the log packet will be sent by pieces of 2 octets
-	// the header remains the same for all writes
-	DPT_HEADER(LOG.fr_out, DPT_SELF_ADDR, DPT_SELF_ADDR, FR_EEP_WRITE, 0, 0, 0, 0)
+	// build the log packet
+	LOG.block.index = LOG.index;
+	time = TIME_get();
+	LOG.block.time[0] = (u8)(time >> 16);
+	LOG.block.time[1] = (u8)(time >>  8);
+	LOG.block.fr = LOG.fr;
 
-	// first, index, orig and cmde fields
-	LOG.fr_out.argv[0] = (u8)((LOG.addr & 0xff00) >> 8);
-	LOG.fr_out.argv[1] = (u8)((LOG.addr & 0x00ff) >> 0);
-	LOG.fr_out.argv[2] = ((LOG.index & 0x0f) << 4) + ((LOG.fr_filter.orig & 0x0f) << 0);
-	LOG.fr_out.argv[3] = LOG.fr_filter.cmde;
-	PT_SPAWN(pt, &LOG.pt3, LOG_write(&LOG.pt3));
+	switch ( LOG.state ) {
+		case LOG_OFF:
+		default:
+			// shall never happen but just in case
+			// loop back for next frame
+			PT_RESTART(pt);
+			break;
 
-	// increment write address
-	LOG.addr += 2;
+		case LOG_EEPROM:
+			// save it to eeprom
+			PT_WAIT_UNTIL(pt, EEP_write(LOG.eeprom_addr, (u8*)&LOG.block, sizeof(log_t)));
 
-	// save time
-	time.full = TIME_get();
-	LOG.fr_out.argv[0] = (u8)((LOG.addr & 0xff00) >> 8);
-	LOG.fr_out.argv[1] = (u8)((LOG.addr & 0x00ff) >> 0);
-	LOG.fr_out.argv[2] = time.part[2];
-	LOG.fr_out.argv[3] = time.part[1];
-	PT_SPAWN(pt, &LOG.pt3, LOG_write(&LOG.pt3));
+			// wait until saving is done
+			PT_WAIT_UNTIL(pt, EEP_is_fini());
+			break;
 
-	// increment write address
-	LOG.addr += 2;
+		case LOG_SDCARD:
+			// save it to sdcard (fill the write buffer)
 
-	// save the first 2 argv
-	LOG.fr_out.argv[0] = (u8)((LOG.addr & 0xff00) >> 8);
-	LOG.fr_out.argv[1] = (u8)((LOG.addr & 0x00ff) >> 0);
-	LOG.fr_out.argv[2] = LOG.fr_filter.argv[0];
-	LOG.fr_out.argv[3] = LOG.fr_filter.argv[1];
-	PT_SPAWN(pt, &LOG.pt3, LOG_write(&LOG.pt3));
+			// wait until saving is done
 
-	// increment write address
-	LOG.addr += 2;
-
-	// save the third argv
-	LOG.fr_out.argv[0] = (u8)((LOG.addr & 0xff00) >> 8);
-	LOG.fr_out.argv[1] = (u8)((LOG.addr & 0x00ff) >> 0);
-	LOG.fr_out.argv[2] = LOG.fr_filter.argv[2];
-	LOG.fr_out.argv[3] = 0xff;	// keep eeprom in erase state
-	PT_SPAWN(pt, &LOG.pt3, LOG_write(&LOG.pt3));
-
-	// address must be rewinded by one
-	// to take into account that only 7 octets were really written
-	LOG.addr--;
+			break;
+	}
 
 	// loop back to treat the next frame to log
 	PT_RESTART(pt);
-
-	PT_END(pt);
-}
-
-
-static PT_THREAD( LOG_main(pt_t* pt) )
-{
-	PT_BEGIN(pt);
-
-	DPT_lock(&LOG.interf);
-
-	// first, find the end of the last recent used log
-	PT_SPAWN(pt, &LOG.pt2, LOG_find_start(&LOG.pt2));
-
-	// then start logging the frames
-	LOG.interf.cmde_mask = _CM(FR_STATE)
-				| _CM(FR_TIME_GET)
-				| _CM(FR_MUX_POWER)
-				| _CM(FR_RECONF_FORCE_MODE)
-				| _CM(FR_MINUT_TAKE_OFF)
-				| _CM(FR_MINUT_DOOR_CMD)
-				| _CM(FR_SURV_PWR_CMD)
-				| MINIMAL_FILTER;
-	PT_SPAWN(pt, &LOG.pt2, LOG_log(&LOG.pt2));
-
-	DPT_unlock(&LOG.interf);
 
 	PT_END(pt);
 }
@@ -356,28 +372,44 @@ static PT_THREAD( LOG_main(pt_t* pt) )
 // log module initialization
 void LOG_init(void)
 {
+	u8 index;
+
 	// init context and fifo
-	PT_INIT(&LOG.pt);
+	PT_INIT(&LOG.log_pt);
 	FIFO_init(&LOG.in_fifo, &LOG.in_buf, NB_FRAMES, sizeof(LOG.in_buf[0]));
 
 	// reset scan start address and index
-	LOG.addr = START_ADDR;
+	LOG.eeprom_addr = EEPROM_START_ADDR;
+	LOG.sdcard_addr = SDCARD_START_ADDR;
 	LOG.index = 0;
 
-	// register own dispatcher call-back
-	// while the begin the log is not found,
-	// no event shall be logged
+	// origin filter blocks every node by default
+	memset(&LOG.orig_filter, 0xff, NB_ORIG_FILTER);
+
+	LOG.state = LOG_OFF;
+
+	// find the start address for this session
+	index = LOG_find_eeprom_start();
+	LOG_find_sdcard_start(index);
+
+	// register to dispatcher
 	LOG.interf.channel = 6;
 	LOG.interf.cmde_mask = MINIMAL_FILTER;
 	LOG.interf.queue = &LOG.in_fifo;
+	LOG.interf.cmde_mask = _CM(FR_STATE)
+				| _CM(FR_MUX_POWER)
+				| _CM(FR_RECONF_FORCE_MODE)
+				| _CM(FR_MINUT_TAKE_OFF)
+				| _CM(FR_MINUT_DOOR_CMD)
+				| _CM(FR_SURV_PWR_CMD)
+				| MINIMAL_FILTER;
 	DPT_register(&LOG.interf);
-
-	LOG.enable = FALSE;
 }
 
 
 // log run method
 void LOG_run(void)
 {
-	(void)PT_SCHEDULE(LOG_main(&LOG.pt));
+	// logging job
+	(void)PT_SCHEDULE(LOG_log(&LOG.log_pt));
 }
